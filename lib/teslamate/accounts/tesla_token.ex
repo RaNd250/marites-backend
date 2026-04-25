b84903ed4e8c984238a03c23d6ff4f127e6dae07
RaNd250 <rand@uns.gr>
2026-04-26 01:30:07 +0300

    feat: add User, InviteCode, RefreshToken, TeslaToken Ecto schemas

---
+++ lib/teslamate/accounts/tesla_token.ex
@@ -0,0 +1,19 @@
+defmodule TeslaMate.Accounts.TeslaToken do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "tesla_tokens" do
+    field :user_id,                :id
+    field :encrypted_refresh_token, TeslaMate.Vault.Encrypted.Binary
+    field :access_token,           :string
+    field :token_expires_at,       :utc_datetime
+    timestamps()
+  end
+
+  def changeset(token, attrs) do
+    token
+    |> cast(attrs, [:user_id, :encrypted_refresh_token, :access_token, :token_expires_at])
+    |> validate_required([:user_id])
+    |> unique_constraint(:user_id)
+  end
+end
