1ac6149112a124a85f605a7b4124ed27fc042323
RaNd250 <rand@uns.gr>
2026-04-30 21:27:18 +0300

    fix: reorder proto modules so Field/Location defined before referencing modules

--- lib/marites/fleet_telemetry/proto.ex
+++ lib/marites/fleet_telemetry/proto.ex
@@ -1,16 +1,22 @@
-defmodule Marites.FleetTelemetry.Payload do
-  use Protobuf, syntax: :proto3
+defmodule Marites.FleetTelemetry.Field do
+  use Protobuf, enum: true, syntax: :proto3
 
-  field :vin,  1, type: :string
-  field :txid, 4, type: :string
-  field :data, 2, repeated: true, type: Marites.FleetTelemetry.Datum
+  field :Unknown,      0
+  field :ChargeState, 12
+  field :GearSelector, 60
+  field :Location,    73
+  field :Odometer,   129
+  field :SentryMode, 167
+  field :Soc,        182
+  field :VehicleSpeed, 183
+  field :ShiftState, 196
 end
 
-defmodule Marites.FleetTelemetry.Datum do
+defmodule Marites.FleetTelemetry.Location do
   use Protobuf, syntax: :proto3
 
-  field :key,   1, type: Marites.FleetTelemetry.Field, enum: true
-  field :value, 2, type: Marites.FleetTelemetry.Value
+  field :latitude,  1, type: :double
+  field :longitude, 2, type: :double
 end
 
 defmodule Marites.FleetTelemetry.Value do
@@ -26,23 +32,17 @@ defmodule Marites.FleetTelemetry.Value do
   field :float_value,    6, type: :float,    oneof: 0
 end
 
-defmodule Marites.FleetTelemetry.Location do
+defmodule Marites.FleetTelemetry.Datum do
   use Protobuf, syntax: :proto3
 
-  field :latitude,  1, type: :double
-  field :longitude, 2, type: :double
+  field :key,   1, type: Marites.FleetTelemetry.Field, enum: true
+  field :value, 2, type: Marites.FleetTelemetry.Value
 end
 
-defmodule Marites.FleetTelemetry.Field do
-  use Protobuf, enum: true, syntax: :proto3
+defmodule Marites.FleetTelemetry.Payload do
+  use Protobuf, syntax: :proto3
 
-  field :Unknown,      0
-  field :ChargeState, 12
-  field :GearSelector, 60
-  field :Location,    73
-  field :Odometer,   129
-  field :SentryMode, 167
-  field :Soc,        182
-  field :VehicleSpeed, 183
-  field :ShiftState, 196
+  field :vin,  1, type: :string
+  field :txid, 4, type: :string
+  field :data, 2, repeated: true, type: Marites.FleetTelemetry.Datum
 end
